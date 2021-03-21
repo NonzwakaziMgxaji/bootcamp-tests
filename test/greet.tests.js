5a4a67bf973858798ef192ed831e227bbb6f554c
NonzwakaziMgxaji <nonzwakazimgxaji@gmail.com>
2021-03-21 15:11:25 +0200

    deleted comments on test files

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -1,8 +1,6 @@
 describe("The greet function" , function(){
     it("should greet Mbali correctly" , function(){
         assert.equal(greet("Mbali"),"Hello, Mbali");
-
-        // assert.deepEqual([2,2],[2,2]);
     });
 
 });
--- test/isFromBellville.tests.js
+++ test/isFromBellville.tests.js
@@ -1,8 +1,6 @@
 describe("The isFromBellville function" , function(){
     it("should return true if registration number is for Bellville or starts with CY" , function(){
         assert.equal(isFromBellville("CY123"),true);
-
-        // assert.deepEqual([2,2],[2,2]);
     });
 
 });
